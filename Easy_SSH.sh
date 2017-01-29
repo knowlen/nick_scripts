b3ece6b821596dfb2514b33740b2862e5931d7a4
Nick A. Knowles <knowlen@LabImage2016-17>
2017-01-28 16:34:21 -0800

    added help flag

--- Easy_SSH.sh
+++ Easy_SSH.sh
@@ -1,3 +1,24 @@
+#a: Nick Knowles (knowlen@wwu.edu)
+#d: Winter 2017
+#
+# This script will connect you to the linux cluster if outside cf.
+# Prompt for a username upon first use (save & recall for future uses).
+# Optionally, you can hand it your username as an arguement (do this if you
+# enter wrong username for first invocation). 
+# Prompts for room and machine number, then connects to that machine.
+
+if [ "$1" == "-h" ]; then
+    echo "Easy SSH (h)"
+    echo "Usage1: ./Easy_SSH.sh"
+    echo "Usage2: ./Easy_SSH.sh <username>"
+    echo "NOTE:"
+    echo "     If having problems connecting, run Usage2 until you type your"
+    echo "     username correctly." 
+    exit
+fi
+
+
+
 # if not in the departmenti
 file="./.SSH_last_uname.txt"
 if [ -z ${1+x} ]; then
