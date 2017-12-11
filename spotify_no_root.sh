00dfb62a62befa8fd9fd5a3d4c30952296d68906
Nick A. Knowles <knowlen@LabImage2016-17>
2017-12-11 00:24:24 -0800

    updated version of spotify

--- spotify_no_root.sh
+++ spotify_no_root.sh
@@ -1,30 +1,28 @@
-# a: Nick Knowles (knowlen@wwu.edu)
-# d: April 2017
+# a: Nick Knowles (knowlen@wwu.edu, knowlen.github.io)
+# d: April 2017 (updated Dec 2017)
 #
 # Summary: This script installs the Spotify application client onto a machine without root. 
-#          -Note: while you can navigate and use all of the features of the spotify application, 
-#                 you must still run the spotify web client (and redirect audio to it) in order
-#                 to get sound.  
 #
-# Usage: $ ./spotify_no_root.sh
-#    (load up spotify web player in Firefox)
-#        $ spotify 
+# Install: $ ./spotify_no_root.sh
+# Usage:   $ spotify 
 #
 
+rm -rf ~/.spotify
 mkdir ~/.spotify
 cd ~/.spotify
+# Last Updated: 21-Nov-2017
+wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.67.582.g19436fa3-28_amd64.deb
+fakeroot sh -c '
+dpkg-deb -R ./*.deb ./
+'
+newpath="export PATH=\$PATH:/home/$(whoami)/.spotify/usr/bin" 
 
-wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.53.758.gde3fc4b2-33_amd64.deb
+# Add executable to the path
+if ! grep -q "$newpath" ~/.bashrc; then
+    echo "$newpath" >> ~/.bashrc;
+fi
 
-tar -xf ./*.deb
-tar -xf ./data*.tar.gz
-touch ~/.bashrc_copy
-touch ~/.bash_prof_copy
-echo "export PATH=\$PATH:/home/$(whoami)/.spotify/usr/bin" >> ~/.bashrc_copy
-echo "export PATH=\$PATH:/home/$(whoami)/.spotify/usr/bin" >> ~/.bash_prof_copy
-cat ~/.bashrc >> ~/.bashrc_copy
-cat ~/.bash_profile >> ~/.bash_prof_copy
-
-mv ~/.bash_prof_copy ~/.bash_profile
-mv ~/.bashrc_copy ~/.bashrc
+if ! grep -q "$newpath" ~/.bash_profile; then
+    echo "$newpath" >> ~/.bash_profile;
+fi
 
